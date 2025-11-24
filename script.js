f0de7b1bf68eb342097bcbbb08f71f1465402f98
jhmm2405-ship-it <jhmm2405@gmail.com>
2025-11-24 03:29:20 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 
+
+// Scroll reveal
+const revealElements = document.querySelectorAll('.reveal');
+
+function reveal() {
+  for (let i = 0; i < revealElements.length; i++) {
+    const windowHeight = window.innerHeight;
+    const elementTop = revealElements[i].getBoundingClientRect().top;
+    const revealPoint = 130;
+
+    if (elementTop < windowHeight - revealPoint) {
+      revealElements[i].classList.add('active');
+    }
+  }
+}
+
+window.addEventListener('scroll', reveal);
+window.addEventListener('load', reveal);
